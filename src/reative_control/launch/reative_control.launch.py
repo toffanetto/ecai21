1581545af18dd4362b066a3cef74a4fd90309baf
Toffanetto <gabrieltoffanetto@gmail.com>
2022-10-31 14:33:08 -0300

    Reative control workspace

---
+++ src/reative_control/launch/reative_control.launch.py
@@ -0,0 +1,42 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_reative_control = get_package_share_directory('reative_control')
+
+    # Gazebo
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
+        )
+    )
+
+    # RViz
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', os.path.join(pkg_reative_control, 'rviz', 'reative_control.rviz')],
+        condition=IfCondition(LaunchConfiguration('rviz'))
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+          'world',
+          default_value=[os.path.join(pkg_reative_control, 'worlds', 'world_2.world'), ''],
+          description='SDF world file'),
+        DeclareLaunchArgument('rviz', default_value='true',
+                              description='Open RViz.'),
+        gazebo,
+        rviz
+    ])
