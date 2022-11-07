985b0b48a00f75138a8c6b5730c9fd11da647b21
Toffanetto <gabrieltoffanetto@gmail.com>
2022-11-07 13:26:31 -0300

    Reactive obstacle avoidance VFinal

--- src/reactive_control/src/reactive_control.cpp
+++ src/reactive_control/src/reactive_control.cpp
@@ -43,11 +43,11 @@ class ReactiveControl : public rclcpp::Node{
 
 	private:
 
+		// Callback para receber os dados do laser
 		void laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
 
+		// Callback para processar os dados de navegação periodicamente
 		void cmd_timer_callback();
-
-		void show_status();
 
 		rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub;
     	rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub;
@@ -76,6 +76,7 @@ void ReactiveControl::cmd_timer_callback(){
 	// 		a -- Desviando para esquerda
 	// 		b -- Desviando para direita
 
+	// Executa o processamento do controle se tiverem chegado novas mensagens do laser
 	if(laser_data_recived){
 
 		std::pair<double,double> nearest_point, first_point, last_point;            
@@ -86,30 +87,44 @@ void ReactiveControl::cmd_timer_callback(){
 		
 		int turn = 1;
 
+		// Processamento dos dados do vetor de pontos do laser
 		for(auto i=0u; i<laser_data->ranges.size();i++){
+
+			// Obtém range do raio atual
 			auto range = laser_data->ranges.at(i);
+			
+			// Calcula ângulo do raio atual
 			theta = laser_data->angle_increment*i + laser_data->angle_min;
 
+			// Caso o range medido for menor que o raio de detecção de objeto estipulado
 			if(range <= RANGE_CRASH){
+
+				// Armazena a primeira posição do obstáculo
 				if(first){ // Inicio da detecção de obstáculo
 					first_point = std::make_pair(range, theta);
 					nearest_point = first_point;
 					last_point = first_point;
 				}
+
+				// Armazena o ponto mais próximo do obstáculo
 				if(range < nearest_point.first) // Definição do ponto mais próximo do robô
 					nearest_point = std::make_pair(range, theta);
 
-				
+				// Armaena o ultimo ponto do obstáculo
 				last_point = std::make_pair(range, theta); // Definição do último ponto conhecido do obstáculo
 			}
 
+			// Calcula se o obstáculo está no trajeto que o robô está fazendo
 			if(range*cos(theta) <= RANGE_CRASH && range*abs(sin(theta)) < ROBOT_WIDTH/2){ // Validação se o robô irá bater no obstáculo que ele encontrou
 				crash = true;
 			}
 
 		}
-
+		
+		// Caso haja obstáculo do trajeto do robô
 		if(crash){
+
+			// Verifica para qual lado é melhor virar com base no menor ângulo entre o robô e os pontos de início e fim do obstáculo
 			if(abs(first_point.second)>abs(last_point.second)){
 				range_max = last_point.first;
 				alpha = last_point.second;
@@ -123,6 +138,7 @@ void ReactiveControl::cmd_timer_callback(){
 				RCLCPP_INFO(this->get_logger(), "Obstacle avoiding by rigth.");
 			}
 
+			// Obtenção do ângulo de correção de tranjetória em relação a extremidade do obstáculo1
 			double cos_theta = ((ROBOT_WIDTH*ROBOT_WIDTH - 2*range_max*range_max)/(- 2*range_max*range_max));
 
 			theta = (abs(cos_theta) > 1) ? copysign(M_PI/2,turn) : copysign(acos(cos_theta),turn);
@@ -132,13 +148,9 @@ void ReactiveControl::cmd_timer_callback(){
 		else
 			RCLCPP_INFO(this->get_logger(), "Normal navigation.");
 
+		// Obtenção das velelocidades lineares e angulares por meio da lei de controle implementada
 		linear_speed = (Kp_Ld*phi < LINEAR_SPEED_MAX*Kp_L) ? LINEAR_SPEED_MAX*Kp_L - Kp_Ld*phi : 0;
 		angular_speed = copysign(phi,turn)*Kp_A;
-
-
-		// 2 -- Seguir parede
-
-
 
 
 		cmd_msg->linear.x = (abs(linear_speed) > LINEAR_SPEED_MAX) ? copysign(LINEAR_SPEED_MAX,linear_speed) : linear_speed;
@@ -150,10 +162,6 @@ void ReactiveControl::cmd_timer_callback(){
 	}
 	else
 		RCLCPP_WARN(this->get_logger(), "No data recived from LaserScan yet.");
-}
-
-void ReactiveControl::show_status(){
-
 }
 
 int main(int argc, char ** argv){
