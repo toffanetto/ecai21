6a5b4b49edf5843e17826c21b64f877be17a9375
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2022-12-04 12:11:44 -0300

    Delete install directory

--- install/potential_field_control/share/potential_field_control/environment/potential_field_control.sh
+++
@@ -1,4 +0,0 @@
-# generated from potential_field_control/env-hooks/reactive_control.sh.in
-
-ament_prepend_unique_value GAZEBO_MODEL_PATH "$AMENT_CURRENT_PREFIX/share/potential_field_control/models"
-ament_prepend_unique_value GAZEBO_RESOURCE_PATH "$AMENT_CURRENT_PREFIX/share/potential_field_control/worlds"
